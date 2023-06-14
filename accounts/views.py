9dd04d14d76ffb6869f5126539988e6dc6d44dbf
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-14 08:20:59 -0400

    simple jwt

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.views import APIView
-
+from .tokens import create_jwt_pair_for_user
 
 
 
@@ -39,11 +39,15 @@ class LoginView(APIView):
 
         user = authenticate(email=email, password=password)
 
+
+
         if user is not None:
+
+            tokens = create_jwt_pair_for_user(user)
+
             response = {
                 "message": "Login Successfully",
-                "token": user.auth_token.key
-
+                "tokens": tokens
             }
             return Response(data=response, status=status.HTTP_200_OK)
         else:
--- djangoRestCourse/settings.py
+++ djangoRestCourse/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+
+from django.conf import settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -61,9 +64,20 @@ REST_FRAMEWORK = {
     "NON_FIELD_ERRORS_KEY": "errors",
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication"
+        "rest_framework.authentication.TokenAuthentication",
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated")
+}
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "SIGNING_KEY": settings.SECRET_KEY,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+
+    # "Bearer <Token>"
 }
 
 
